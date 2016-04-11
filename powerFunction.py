c80fc4e374f60c36348a59a8e5c95d5ef54ee347
Jared <Jared@Jareds-MacBook-Pro.local>
2016-04-11 15:01:21 -0400

    recursive version of the function

--- powerFunction.py
+++ powerFunction.py
@@ -3,16 +3,51 @@
 
 def power(base, exponent):
 
-	answer = base
-	counter = 1
-	while exponent > counter:	
-		counter += 1
-		answer = answer * base
-
-	return answer
+	zero = "1"
 	
-base = int(input("Type a base number. "))
-exponent = int(input("Type an exponent. "))
+	if exponent < 1:
+		return zero
+	
+	if exponent == 1:
+		return base
+	
+	
+	result = (base) * power(base, exponent - 1)
+
+	return result
+
+
+#-----------------------------------------------------------------------------------------
+	
+def int_input(question):
+	
+	"""Asks the user for an int, and let them try again."""
+	
+	answer = input(question)
+	
+	try:
+		#terminating case
+		answer = int(answer)
+		return answer
+	
+	except ValueError:
+		return int_input("That was not an integer; try again. ")
+
+def float_input(question):
+
+	answer = input(question)
+	
+	try:
+		answer = float(answer)
+		return answer
+	
+	except ValueError:
+		return float_input("That was not a float, try again. ")
+#-----------------------------Main Program---------------------------
+
+
+base = float_input("Type a base number. ")
+exponent = int_input("Type an exponent. ")
 
 if exponent == 0:
 	answer = 1
@@ -21,3 +56,4 @@ if exponent == 0:
 else:
 	answer = power(base, exponent)
 	print("The answer is {}.".format(answer))
+	
